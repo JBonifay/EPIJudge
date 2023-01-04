a36c458c8122d0339a0bbfc97c5d68bac07fef9b
Joffrey B <joffreybonifay83@gmail.com>
2023-01-04 08:46:33 +0100

    add solution for parity

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    // 0101 1010 => 1
+    // 0101 1110 => 0
+    short parity = 0;
+    while (x != 0) {
+      parity ^= 1; // set bit to 0 or 1 
+      x &= (x - 1); // remove the lowest set bit
+    }
+
+    return parity;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
