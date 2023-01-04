5c8b04c4c18231ecb9b11f44185af51b3119f3ea
Joffrey B <joffreybonifay83@gmail.com>
2023-01-04 08:36:26 +0100

    add solution for countBits

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -1,19 +1,25 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
-
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+    short sum = 0;
+    while (x != 0) {
+      x = x & (x - 1); // Erase the lowest set bit
+      sum++;
+    }
+    return sum;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "CountBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
