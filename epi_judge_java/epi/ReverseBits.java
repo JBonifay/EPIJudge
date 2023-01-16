0bb83121c54a44362a53958fe8251dd6c2a4d325
Joffrey B <joffreybonifay83@gmail.com>
2023-01-16 09:01:42 +0100

    add solution for reverseBits using lookup table

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -1,18 +1,55 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ReverseBits {
+
+  private static final long[] lookupTable = new long[(1 << 16)];
+
+  static {
+    for (int i = 0; i < (1 << 16); i++) {
+      lookupTable[i] = reverse(i);
+    }
+  }
+
+  private static int reverse(int numberToReverse) {
+    final int integerLength = 15;
+    for (int left = 0, right = integerLength; left < right; left++, right--) {
+      if (needToReverse(numberToReverse, left, right)) {
+        numberToReverse = reverseBits(numberToReverse, left, right);
+      }
+    }
+    return numberToReverse;
+  }
+
+  private static boolean needToReverse(final int x, final int i, final int j) {
+    return ((x >>> i) & 1) != ((x >>> j) & 1);
+  }
+
+  private static int reverseBits(int x, final int i, final int j) {
+    int mask = (1 << i) | (1 << j);
+    x ^= mask;
+    return x;
+  }
+
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+    int bitMask = 0xFFFF;
+    int maskLength = 16;
+
+    return lookupTable[(int) (x >>> (3 * maskLength) & bitMask)] << (0 * maskLength) |
+           lookupTable[(int) (x >>> (2 * maskLength) & bitMask)] << (1 * maskLength) |
+           lookupTable[(int) (x >>> (1 * maskLength) & bitMask)] << (2 * maskLength) |
+           lookupTable[(int) (x >>> (0 * maskLength) & bitMask)] << (3 * maskLength);
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "ReverseBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
--- epi_judge_java_solutions/epi/ReverseBits.java
+++ epi_judge_java_solutions/epi/ReverseBits.java
@@ -6,8 +6,8 @@ import epi.test_framework.GenericTest;
 public class ReverseBits {
   private static long[] precomputedReverse = new long[(1 << 16)];
 
-  private static long reverseBits(long x, int n) {
-    for (int i = 0, j = n; i < j; ++i, --j) {
+  private static long reverseBits(long x, ) {
+    for (int i = 0, j = 15; i < j; ++i, --j) {
       x = SwapBits.swapBits(x, i, j);
     }
     return x;
@@ -15,7 +15,7 @@ public class ReverseBits {
 
   static {
     for (int i = 0; i < (1 << 16); ++i) {
-      precomputedReverse[i] = reverseBits(i, 15);
+      precomputedReverse[i] = reverseBits(i);
     }
   }
 
@@ -25,10 +25,8 @@ public class ReverseBits {
     final int MASK_SIZE = 16;
     final int BIT_MASK = 0xFFFF;
     return precomputedReverse[(int)(x & BIT_MASK)] << (3 * MASK_SIZE) |
-        precomputedReverse[(int)((x >>> MASK_SIZE) & BIT_MASK)]
-            << (2 * MASK_SIZE) |
-        precomputedReverse[(int)((x >>> (2 * MASK_SIZE)) & BIT_MASK)]
-            << MASK_SIZE |
+        precomputedReverse[(int)((x >>> MASK_SIZE) & BIT_MASK)] << (2 * MASK_SIZE) |
+        precomputedReverse[(int)((x >>> (2 * MASK_SIZE)) & BIT_MASK)] << MASK_SIZE |
         precomputedReverse[(int)((x >>> (3 * MASK_SIZE)) & BIT_MASK)];
   }
 
