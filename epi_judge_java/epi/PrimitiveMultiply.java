112aa01426eac64499abbdf13214d29c9da6a4ae
Joffrey B <joffreybonifay83@gmail.com>
2023-01-20 14:37:02 +0100

    add solution for PrimitiveMultiply

--- epi_judge_java/epi/PrimitiveMultiply.java
+++ epi_judge_java/epi/PrimitiveMultiply.java
@@ -1,18 +1,35 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class PrimitiveMultiply {
   @EpiTest(testDataFile = "primitive_multiply.tsv")
   public static long multiply(long x, long y) {
-    // TODO - you fill in here.
-    return 0;
+    // 1101 * 1001
+    long sum = 0;
+    while (x != 0) {
+      if ((x & 1) != 0) {
+        sum = add(sum, y);
+      }
+      x >>>= 1;
+      y <<= 1;
+    }
+
+    return sum;
   }
+
+  public static long add(long a, long b) {
+    return b == 0 ? a : add(a ^ b, (a & b) << 1);
+  }
+
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "PrimitiveMultiply.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
